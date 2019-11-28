067ec1563cab251ebce6b1d4bfeda76e13405fb2
Mohan Krishna <mohankrishna211@gmail.com>
2019-11-28 11:31:24 +0530

    second commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'blog/temaplates'), os.path.join(BASE_DIR, 'users/temaplates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
